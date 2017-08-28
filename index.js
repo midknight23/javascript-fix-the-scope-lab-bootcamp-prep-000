2bf069f6bfd397734fd9b7d224bc3d41cfc479ef
Robert Fischer <lilbfisch@gmail.com>
2017-08-28 14:58:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var animal = 'dog'
 
 function myAnimal() {
   return animal
-
+}
 
 function yourAnimal() {
   // How can we make sure that this function
@@ -10,7 +10,7 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
-}
+
 
 function add2(n) {
   const two = 2
